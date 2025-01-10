decf270509bbe4194ebdf3b975284ba69c6b9edb
智慧派大星 <118578869+momo97620@users.noreply.github.com>
2025-01-10 16:32:29 +0800

    Save wtl.sh.

--- wtl.sh
+++ wtl.sh
@@ -855,11 +855,11 @@ show_main_menu() {
     
     echo -e "${YELLOW}请选择要执行的任务：${NC}"
 
-    echo -e "  ${BLUE}1.${NC} ${PINK}♥${NC} ${NC}搭建hy2节点${NC}    ${BLUE}09${NC} ${PINK}♥${NC} ${LIGHTCYAN}系统信息查询${NC}"
+    echo -e "  ${BLUE}1.${NC} ${PINK}♥${NC} ${NC}搭建hy2节点${NC}     ${BLUE}09${NC} ${PINK}♥${NC} ${LIGHTCYAN}系统信息查询${NC}"
 
 echo "-------------------------------------"
 
-echo -e "  ${BLUE}2.${NC} ${PINK}♥${NC} ${NC}UFW 防火墙${NC}         ${BLUE}10${NC} ${PINK}♥${NC} ${LIGHTCYAN}IPv4/6优先${NC}"
+echo -e "  ${BLUE}2.${NC} ${PINK}♥${NC} ${NC}UFW 防火墙${NC}     ${BLUE}10${NC} ${PINK}♥${NC} ${LIGHTCYAN}IPv4/6优先${NC}"
 
 echo "-------------------------------------"
 
@@ -879,11 +879,11 @@ echo -e "  ${BLUE}6.${NC} ${PINK}♥${NC} ${GREEN}一键配置WARP${NC}    ${BLU
 
 echo "-------------------------------------"
 
-echo -e "  ${BLUE}7.${NC} ${PINK}♥${NC} ${GREEN}一键BBR加速          ${BLUE}15${NC} ${PINK}♥${NC} ${RED}主机名颜色${NC}"
+echo -e "  ${BLUE}7.${NC} ${PINK}♥${NC} ${GREEN}一键BBR加速       ${BLUE}15${NC} ${PINK}♥${NC} ${RED}主机名颜色${NC}"
 
 echo "-------------------------------------"
 
-echo -e "  ${BLUE}8.${NC} ${PINK}♥${NC} ${LIGHTCYAN}Docker项目 ▶${NC}  ${BLUE}16${NC} ${PINK}♥${NC} ${RED}测试流媒体${NC}"
+echo -e "  ${BLUE}8.${NC} ${PINK}♥${NC} ${LIGHTCYAN}Docker项目 ▶${NC}    ${BLUE}16${NC} ${PINK}♥${NC} ${RED}测试流媒体${NC}"
 
 echo "-------------------------------------"
 
