2100adc1c3b1644575aed73d48d0e36112fdc40e
智慧派大星 <118578869+momo97620@users.noreply.github.com>
2025-01-14 07:27:52 +0800

    Save wtl.sh.
    wtl.sh

--- wtl.sh
+++ wtl.sh
@@ -129,7 +129,18 @@ install_docker() {
 }
 
 set_ip_priority() {
+    # 检查是否以 root 用户运行
+    if [ "$EUID" -ne 0 ]; then
+        echo "请以 root 用户运行此脚本！"
+        exit 1
+    fi
+
     check_current_priority() {
+        if [ ! -f /etc/gai.conf ]; then
+            echo "当前优先级：IPv4 和 IPv6 同时启用（默认）"
+            return
+        fi
+
         if grep -q "label ::ffff:0:0/96  2" /etc/gai.conf; then
             echo "当前优先级：IPv4 优先"
         elif grep -q "label ::/0  1" /etc/gai.conf; then
@@ -178,7 +189,7 @@ set_ip_priority() {
         echo "2. 优先使用 IPv6"
         echo "3. 同时启用 IPv4 和 IPv6"
         echo "==============================="
-        read -p "请输入选项 [1/2/3，0]：" choice
+        read -p "请输入选项 [1/2/3]：" choice
         set_priority "$choice"
         echo "设置完成！按任意键退出..."
         read -n 1 -s -r
