b277acd7ce9bbcaffc98f707d031e8b9958a8c0c
智慧派大星 <118578869+momo97620@users.noreply.github.com>
2025-01-14 15:19:17 +0800

    Save wtl.sh.

--- wtl.sh
+++ wtl.sh
@@ -817,7 +817,7 @@ show_main_menu() {
     clear
     # 定义颜色
     LIGHTCYAN='\033[1;36m'  # 明亮的青色
-    PINK='\033[38;5;
+    PINK='\033[38;5;198m'     # 深粉色
     NC='\033[0m'           # 重置颜色
 
     echo -e "${LIGHTCYAN}"
